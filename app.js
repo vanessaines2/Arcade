c9791448d121ea8cadaaa104e6c099614237c45b
Vanessaines2 <Vanessaines2@icloud.com>
2023-03-13 12:16:50 -0400

    checks for winner- row,col,dia

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const nameBtnO = document.querySelector("#nameBtnO");
 const nameX = document.querySelector("#nameX");
 const nameO = document.querySelector("#nameO");
 const playerName = document.querySelector(".player");
-const playerSelected = document.querySelector("#player-select");
+const playerOption = document.querySelector("#player-option");
 
 const gameState = {
   players: [
@@ -53,22 +53,95 @@ nameBtnO.addEventListener("click", function () {
   renderScoreBoard();
 });
 
-function initialState() {
-  playerSelected.addEventListener("click", () => {
-    //select first player, make them go first
-  });
-  //then for each cell they click on pass by a function
-  cell.forEach((cell) => {
-    cell.addEventListener("click", onBoardClick);
-  });
+playerOption.addEventListener("change", (event) => {
+  console.log(event.target.value);
+  if (event.target.value === "Player O") {
+    gameState.currentPlayerIndex = 0;
+  } else {
+    gameState.currentPlayerIndex = 1;
+  }
+});
+function switchPlayer() {
+  if (gameState.currentPlayerIndex === 0) {
+    gameState.currentPlayerIndex = 1;
+  } else {
+    gameState.currentPlayerIndex = 0;
+  }
 }
-function onBoardClick() {
-  console.log("clicked");
-  //first player selected
-  //cell clicked on --
-  // if empty insert their value
-  //check if win or draw
-  // else switch
+function onBoardClick(e) {
+  //insert player selected
+  const row = e.target.id[0];
+  const col = e.target.id[1];
+  console.log(row, col);
+  if (gameState.board[row][col] === "") {
+    gameState.board[row][col] = gameState.currentPlayerIndex === 0 ? "O" : "X";
+    renderBoard();
+    checkWin();
+    switchPlayer();
+  }
 }
+board.addEventListener("click", onBoardClick);
+
+function renderBoard() {
+  console.log("click");
+  for (let i = 0; i < gameState.board.length; i++) {
+    for (let j = 0; j < gameState.board[i].length; j++) {
+      const square = document.getElementById(`${i}${j}`);
+      square.innerText = gameState.board[i][j];
+    }
+  }
+}
+function checkWin() {
+  checkRow();
+  checkColumn();
+  checkDiagonals();
+}
+
+function checkRow() {
+  for (let i = 0; i < gameState.board.length; i++) {
+    if (
+      gameState.board[i][0] !== "" &&
+      gameState.board[i][0] === gameState.board[i][1] &&
+      gameState.board[i][1] === gameState.board[i][2]
+    ) {
+      console.log("winner!!!!");
+    }
+  }
+}
+
+function checkColumn() {
+  for (let j = 0; j < gameState.board.length; j++) {
+    if (
+      gameState.board[j][0] !== "" &&
+      gameState.board[0][j] === gameState.board[1][j] &&
+      gameState.board[1][j] === gameState.board[2][j]
+    ) {
+      console.log("winnner!");
+    }
+  }
+}
+
+function checkDiagonals() {
+  for (let j = 0; j < gameState.board.length; j++) {
+    if (
+      gameState.board[j][0] !== "" &&
+      gameState.board[0][0] === gameState.board[1][1] &&
+      gameState.board[1][1] === gameState.board[2][2]
+    ) {
+      console.log("Winner!");
+    } else if (
+      gameState.board[j][0] !== "" &&
+      gameState.board[0][2] === gameState.board[1][1] &&
+      gameState.board[1][1] === gameState.board[2][0]
+    ) {
+      console.log("win!");
+    }
+  }
+}
+function checkDraw() {}
 function resetBoard() {}
 restartButton.addEventListener("click", resetBoard);
+
+function winningMessage() {
+  alert("Winner!!!!!");
+}
